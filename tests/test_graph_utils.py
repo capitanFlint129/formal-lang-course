302b8387f89676e955bf7e55cd43838ed55783e0
capitanFlint <tekkengod@list.ru>
2023-02-23 20:57:13 +0300

    add tests

---
+++ tests/test_graph_utils.py
@@ -0,0 +1,15 @@
+from project import grapth_utils
+
+
+def test_get_graph_info():
+    graph = grapth_utils.get_graph_by_name("gzip")
+    assert grapth_utils.get_number_of_nodes(graph) == 2687
+    assert grapth_utils.get_number_of_edges(graph) == 2293
+    assert set(grapth_utils.get_labels(graph)) == {"a", "d"}
+
+
+def test_two_cycle_graph():
+    graph = grapth_utils.create_two_cycles_graph(9, 4, ["a", "b"])
+    assert graph.number_of_nodes() == 14
+    assert graph.number_of_edges() == 15
+    assert set(grapth_utils.get_labels(graph)) == {"a", "b"}
