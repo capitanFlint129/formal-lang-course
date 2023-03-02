1de7911475c162ad0ddf10e943a7ed16f52c0099
capitanFlint129 <tekkengod@list.ru>
2023-03-02 21:32:55 +0300

    add implementation

---
+++ project/automata.py
@@ -0,0 +1,42 @@
+from pyformlang.regular_expression import Regex
+from pyformlang.fst.fst import FST
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    State,
+    Symbol,
+    NondeterministicFiniteAutomaton,
+)
+from networkx import MultiDiGraph
+
+from typing import Optional, Iterable
+
+
+def get_deterministic_automata_from_regex(regex: Regex) -> DeterministicFiniteAutomaton:
+    nfa = regex.to_epsilon_nfa()
+    dfa = nfa.to_deterministic()
+    dfa_minimum = dfa.minimize()
+    return dfa_minimum
+
+
+def get_nondeterministic_automata_from_graph(
+    graph: MultiDiGraph,
+    start_states: Optional[Iterable] = None,
+    final_states: Optional[Iterable] = None,
+) -> NondeterministicFiniteAutomaton:
+    nfa = NondeterministicFiniteAutomaton.from_networkx(graph)
+
+    if start_states is None:
+        for state in nfa.states:
+            nfa.add_start_state(state)
+    else:
+        for state in start_states:
+            nfa.add_start_state(state)
+
+    if final_states is None:
+        for state in nfa.states:
+            nfa.add_final_state(state)
+    else:
+        for state in final_states:
+            nfa.add_final_state(state)
+
+    return nfa
