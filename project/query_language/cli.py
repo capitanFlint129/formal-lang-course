478e35028d5e49c95f442100089b620b3e18978b
capitanFlint <tekkengod@list.ru>
2023-06-03 13:25:26 +0300

    fix cli

--- project/query_language/cli.py
+++ project/query_language/cli.py
@@ -5,6 +5,6 @@ from project.query_language.interpreter.interpreter import Interpreter
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("You need to specify path to program", file=sys.stderr)
-
-    interpreter = Interpreter()
-    interpreter.execute_from_path(sys.argv[1])
+    else:
+        interpreter = Interpreter()
+        interpreter.execute_from_path(sys.argv[1])
